b0283bca0d55299b18f401d45d3d7b8fbb72f631
= <=>
2022-03-07 13:03:44 +0700

    function stateless

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 //import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const Hello = () =>{
-  return <p>hello</p>
-}
+function HelloComponent(){
+  return HelloComponent
+ReactDOM.render(<HelloComponent />, document.getElementById('root'));
+};
 //ReactDOM.render(<App />, document.getElementById('root'));
 
-ReactDOM.render(<Hello />, document.getElementById('root'));
+//ReactDOM.render(<Hello />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
