42fd1cb17b1eabb0983f5d70473a2ca1ed6d7803
= <=>
2022-03-16 14:47:20 +0700

    week4

--- src/index.js
+++ src/index.js
@@ -1,30 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { render } from '@testing-library/react';
-import HelloComponent from './HelloComponent';
-import FormLogin from './formLogin';
-import LifecycleComponent from './LifecycleComponent'
-//import Image from './Image';
+import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render( <
-    React.StrictMode >
-    <App / >
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+import BlogPost from "./container/BlogPost/BlogPost";
 
-// class StateFullComponent extends React.Component {
-//     render() {
-//         return <p>State Full Component</p>
-//     }
-// }
+ReactDOM.render( <BlogPost />, document.getElementById('content'));
 
-ReactDOM.render( < LifecycleComponent /> , document.getElementById('root'));
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+serviceWorker.unregister();
